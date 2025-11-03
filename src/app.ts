792233814579d0e161841ee3909d32167f91e0ed
willian <will.oliveira.2408@gmail.com>
2025-11-03 07:40:05 -0300

    removido um problema de sintaxe

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: 'https://willian-ribeiro.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
 
--- src/router/goodbyepdfRouter.ts
+++ src/router/goodbyepdfRouter.ts
@@ -9,7 +9,12 @@ const router = express.Router();
 const app = express();
 
 app.use(express.json())
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://willian-ribeiro.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 
 
 //configuração do multer para tratar arquivos
